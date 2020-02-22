df12d0ae1fd8e0b5bc92b8f631f57747e0948052
Edward Wang <edwardwang2003@gmail.com>
2020-02-22 14:43:37 -0500

    Fixed quicksort again

--- QuickSort.py
+++ QuickSort.py
@@ -2,7 +2,7 @@ def partition(array, start, end):
     pivot_value = array[end]
     left_bound = start - 1
     for i in range(start, end):
-        if array[i] < pivot_value:
+        if array[i] <= pivot_value:
             left_bound += 1
             (array[i], array[left_bound]) = (array[left_bound], array[i])
     (array[end], array[left_bound + 1]) = (array[left_bound + 1], array[end])
@@ -14,10 +14,11 @@ def quick_sort(array, start=0, end=-1):
         end = len(array) - 1
     if start >= end:
         return
+    print(array, (start, end))
     pivot = partition(array, start, end)
     quick_sort(array, start, pivot - 1)
     quick_sort(array, pivot + 1, end)
     return array
 
 
-print(quick_sort([3, 4, 5, 1, 2, 13, 214, 5241, 153, 635, 23]))
+print(quick_sort([3, 4, 5, 1, 2, 1, 1, 1, 1, 1, 13, 214, 5241, 153, 635, 23]))
