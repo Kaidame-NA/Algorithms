f893fa4137ff0c316a66b86c517f5a9a32b21653
Edward Wang <edwardwang2003@gmail.com>
2020-02-22 15:05:27 -0500

    Make code concise

--- QuickSort.py
+++ QuickSort.py
@@ -9,27 +9,18 @@ def partition(array, start, end):
     return left_bound + 1
 
 
-def quick_sort(array, start=None, end=None):
-    if start is not None and end is not None:
+def quick_sort(array, start=0, end=None):
+    if end is not None:
         if start < end:
             pivot = partition(array, start, end)
             quick_sort(array, start, pivot - 1)
             quick_sort(array, pivot + 1, end)
     else:
-        if start is None and end is None:
-            pivot = partition(array, 0, len(array) - 1)
-            quick_sort(array, 0, pivot - 1)
-            quick_sort(array, pivot + 1, len(array) - 1)
-        elif start is None:
-            pivot = partition(array, 0, end)
-            quick_sort(array, 0, pivot - 1)
-            quick_sort(array, pivot + 1, end)
-        else:
-            pivot = partition(array, start, len(array) - 1)
-            quick_sort(array, start, pivot - 1)
-            quick_sort(array, pivot + 1, len(array) - 1)
+        pivot = partition(array, start, len(array) - 1)
+        quick_sort(array, start, pivot - 1)
+        quick_sort(array, pivot + 1, len(array) - 1)
 
     return array
 
 
-print(quick_sort([3, 4, 5, 1, 2, 1, 1, 1, 1, 13],end=5))
+print(quick_sort([3, 4, 5, 1, 2, 1, 1, 1, 1, 13],5))
